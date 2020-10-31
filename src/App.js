ff2013c3a8062a956301d16129962bcc1da0e6cb
Slawek T <62992201+stlrczk@users.noreply.github.com>
2020-10-31 14:46:40 +0100

    Add nosleep

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback } from "react";
+import NoSleep from "nosleep.js";
 import PlayerCard from "./components/PlayerCard";
 
 import "./styles.css";
 
+let noSleep = new NoSleep();
+
 export default function App() {
+  const enableNoSleep = useCallback(() => {
+    document.removeEventListener("click", enableNoSleep, false);
+    noSleep.enable();
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("click", enableNoSleep, false);
+    return () => {
+      document.removeEventListener("click", enableNoSleep);
+    };
+  }, []);
   return (
     <div className="App">
       <PlayerCard name="Player 1" />
